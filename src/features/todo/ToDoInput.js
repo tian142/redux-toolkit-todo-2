260e090010dde5e4f207ade370149c7170b92621
Tian <tian.qiu142@gmail.com>
2021-02-12 23:08:28 -0800

    completed add input logic using dispatch

--- src/features/todo/ToDoInput.js
+++ src/features/todo/ToDoInput.js
@@ -1,12 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
+
+import { addTodo } from "./todoSlice"
+import { useDispatch } from "react-redux"
 
 export const ToDoInput = () => {
+  const [input, setInput] = useState("")
+  const dispatch = useDispatch()
+  const onInpitChange = (e) => setInput(e.target.value)
+  const onInputSubmit = () => dispatch(addTodo(input))
+
   return (
     <div>
       <h1>ToDo List</h1>
       <form>
-        <input className="spandis" />
-        <button type="button">Add ToDo</button>
+        <input className="spandis" value={input} onChange={onInpitChange} />
+        <button type="button" onClick={onInputSubmit}>
+          Add ToDo
+        </button>
       </form>
     </div>
   )
